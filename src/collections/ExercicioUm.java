c1957b19cc1cc4d7de1344ef3499b3c4d05c0711
Vinicius Bastos <vclemente2@hotmail.com>
2023-09-05 08:13:35 -0300

    Conclusão exercício um.

---
+++ src/collections/ExercicioUm.java
@@ -0,0 +1,33 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExercicioUm {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> cores= new ArrayList<String>();
+		
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Digite a " + (i + 1) + "ª cor");
+			cores.add(scanner.next());
+		}
+
+		System.out.println("Lista todas as cores:\n");
+		for(String cor: cores) {
+			System.out.println(cor);
+		}
+		
+		cores.sort(null);
+		
+		
+		System.out.println("\nOrdenar as cores:\n");
+		for(String cor: cores) {
+			System.out.println(cor);
+		}
+		
+		
+		scanner.close();
+	}
+}
