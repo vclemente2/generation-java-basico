1551dde95b157b2fe394b95e0862d84f2b4edf71
Vinicius Bastos <vclemente2@hotmail.com>
2023-09-05 09:39:10 -0300

    Conclusão exercício dois.

---
+++ src/collections/ExercicioDois.java
@@ -0,0 +1,36 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExercicioDois {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		int numero, indice;
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		numeros.add(2);
+        numeros.add(5);
+        numeros.add(1);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(9);
+        numeros.add(7);
+        numeros.add(8);
+        numeros.add(10);
+        numeros.add(6);
+	
+		System.out.println("Digite um número:");
+		numero = scanner.nextInt();
+		
+		indice = numeros.indexOf(numero);
+	
+		if(indice == -1)
+			System.out.println("O Número " + numero + " não foi encontrado");
+		else
+			System.out.println("O Número " + numero + " está localizado na posição " + indice);
+		
+		scanner.close();
+	}
+}
