be948af6d0b2a8d932149754a7dc3ab3298803c9
Vinicius Bastos <vclemente2@hotmail.com>
2023-09-05 09:45:17 -0300

    Conclusão exercício três.

---
+++ src/collections/ExercicioTres.java
@@ -0,0 +1,32 @@
+package collections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ExercicioTres {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        Set<Integer> setNumeros = new HashSet<Integer>();
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Digite um número: ");
+             setNumeros.add(leitor.nextInt());
+        }
+
+        Iterator<Integer> isetNumeros = setNumeros.iterator();
+
+        System.out.println("\nLista de todos os números com o Iterator:");
+
+        while (isetNumeros.hasNext()) {
+            System.out.println(isetNumeros.next());
+        }
+
+        leitor.close();
+    }
+
+}
