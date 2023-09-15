5f918fc2a7b0310b07f01f814ffe2760729fc09e
Vinicius Bastos <vclemente2@hotmail.com>
2023-09-15 13:50:03 -0300

    Conclusão exercício dois.

--- src/poo/exercicio_dois/TestaFuncionario.java
+++ src/poo/exercicio_dois/TestaFuncionario.java
@@ -1,12 +1,18 @@
 package poo.exercicio_dois;
 
+import java.util.Optional;
+
 public class TestaFuncionario {
     public static void main(String[] args) {
         try {
         Gerente funcionarioUm = new Gerente("Vinicius", "0001", "TI", 31, 10);
         Gerente funcionarioDois = new Gerente("Gabi", "0002", "RH", 28, 2);
         
-        Vendedor funcionarioTres = new Vendedor("Fulano", "0003", "TI", 25, 0.01f);
+        String[] dadosFuncionarioTres = {"Fulano", "0003", null};
+        Optional<String> verificaSetor = Optional.ofNullable(dadosFuncionarioTres[2]);
+        dadosFuncionarioTres[2] = verificaSetor.orElse("TI");
+        
+        Vendedor funcionarioTres = new Vendedor(dadosFuncionarioTres[0], dadosFuncionarioTres[1], dadosFuncionarioTres[2], 25, 0.01f);
         Vendedor funcionarioQuatro = new Vendedor("Fulana", "0004", "TI", 19, 0.02f);
 
   
